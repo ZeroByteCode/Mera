114cd3fac01a5870646865ba2e4b5f5ade4e9aa5
ZeroByteCode <124530272+ZeroByteCode@users.noreply.github.com>
2025-04-11 08:08:38 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function sayHello() {
+      let answer = window.prompt("Bati na po ba kita?🥺 Yes or No 🥺","");
+      let arranswer = [answer.toLowerCase()];
+      if(arranswer.includes("yes")){
+        alert("I love you 🥺\nHappy Monthsarry 🥺");
+      }else{
+       alert("Sorry na 😞");
+      }
+    }
